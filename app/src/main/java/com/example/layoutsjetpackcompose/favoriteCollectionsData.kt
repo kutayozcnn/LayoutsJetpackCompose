a55cce2cdbd0a2f59597b79d3d82bd671d18bf4a
kutayozcnn <80099601+kutayozcnn@users.noreply.github.com>
2022-10-05 15:16:34 +0300

    Layouts Tutorial

---
+++ app/src/main/java/com/example/layoutsjetpackcompose/favoriteCollectionsData.kt
@@ -0,0 +1,10 @@
+package com.example.layoutsjetpackcompose
+
+val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
